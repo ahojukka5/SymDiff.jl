e54a5f76f4bd8b90f34decf69dea00e2dec5b576
Jukka Aho <ahojukka5@gmail.com>
2020-06-22 21:27:53 +0700

    Refactor tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Test
 using SymDiff: differentiate
 
-@test differentiate(1, :x) == 0
+@testset "Test SymDiff.jl" begin
+    @testset "Test differentiate of constant" begin
+        @test differentiate(1, :x) == 0
+        @test differentiate(1.0, :x) == 0    
+        @test differentiate(1//2, :x) == 0
+    end
+end
 
-@test differentiate(1.0, :x) == 0
