acff470808aa27fd46ec956bfaa2492dc7d25ac8
Jukka Aho <ahojukka5@gmail.com>
2020-06-22 22:35:04 +0700

    Add simplification of expression

--- src/simplify.jl
+++ src/simplify.jl
@@ -1,4 +1,14 @@
 """
+    simplify(expr::Expr)
+
+Simplify expression.
+"""
+function simplify(ex::Expr)
+    @assert ex.head == :call
+    return simplify(Val{ex.args[1]}, ex)
+end
+
+"""
     simplify(f::Union{Number, Symbol})
 """
 function simplify(f::Union{Number, Symbol})
